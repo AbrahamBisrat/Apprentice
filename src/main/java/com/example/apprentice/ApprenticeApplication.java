f290edfe40688d46aaad1b263abc66e979e8149e
Abraham Bisrat <abrahammehari0@gmail.com>
2022-04-04 15:12:25 -0500

    Starter boilerplate

---
+++ src/main/java/com/example/apprentice/ApprenticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.apprentice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApprenticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApprenticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apprentice/ApprenticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apprentice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApprenticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
